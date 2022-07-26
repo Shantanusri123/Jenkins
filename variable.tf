85cd8425c6dbe462551a3eeacd2228aebc13f152
Shantanusri123 <105036664+Shantanusri123@users.noreply.github.com>
2022-07-26 13:34:02 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,19 +1,10 @@
-#AWS Config
-
-variable "aws_access_key" {
-default = "AKIAR7PCU5YC6J5LXEB"
-}
-variable "aws_secret_key" {
-default = "j77PJw9ECIgg8a9N74U+L7iT9AiNMeFqmkDnQKi"
-}
-variable "aws_region" {
-default = "us-east-1"
-}
 variable "bucket_name" {
  type = string
 }
 
 variable "acl_value" {
  type = string
- default = "private"
+}
+
+variable "aws_region" {
 }

73dbdf96177c3c17b612ea3326fb15b5d88c7622
Shantanusri123 <105036664+Shantanusri123@users.noreply.github.com>
2022-07-27 18:45:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@
 resource "aws_instance" "ec2instance"{
   ami = var.ami
   instnace_type = t2.micro
-  security_groups = [var.security_group.id]
+  security_groups = [var.security_group]
  tags = {
    Name = var.instance_name
  }

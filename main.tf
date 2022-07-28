200162c8a457cb8bc7db8956ef71a9dd61edfc7e
Shantanusri123 <105036664+Shantanusri123@users.noreply.github.com>
2022-07-28 15:29:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@
 #   acl = var.acl_value
 # }
 
-resource "aws_instance" "ec2instance"{
-  ami = var.ami
-  instnace_type = t2.micro
-  security_groups = var.security_group
- tags = {
-   Name = var.instance_name
- }
-}  
+# resource "aws_instance" "ec2instance"{
+#   ami = var.ami
+#   instnace_type = t2.micro
+#   security_groups = var.security_group
+#  tags = {
+#    Name = var.instance_name
+#  }
+# }  
  
